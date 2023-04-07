5bd105ad17adc5658eb57241d4437c470496c5cc
iorin006 <108205818+iorin006@users.noreply.github.com>
2023-04-08 00:11:09 +0900

    Add files via upload

--- ConfigLoader.bat
+++ ConfigLoader.bat
@@ -3,14 +3,14 @@
 title ConfigDownloader
 
 SET INPUTSTR=
-SET name=
 
-SET /P name="pcのユーザーネームを記入してください"
+echo %username%
+
 echo ファイルをドラッグアンドドロップしてエンターを押してください
 
 SET /P INPUTSTR=""
 
-copy %INPUTSTR% C:\Users\%name%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\RoamingState\project-halcyon\configurations
+copy %INPUTSTR% C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\RoamingState\project-halcyon\configurations
 
 echo 完了しました
 pause
