43cdfd59c1c2f7e0457088fc71fe6a9d2c71ecde
iorin006 <108205818+iorin006@users.noreply.github.com>
2023-10-02 14:17:20 +0900

    Add files via upload

--- ConfigLoader.bat
+++ ConfigLoader.bat
@@ -1,6 +1,7 @@
 @echo off
 
 title ConfigDownloader
+echo 				Vector専用
 
 SET INPUTSTR=
 
@@ -10,7 +11,7 @@ echo ファイルをドラッグアンドドロップしてエンターを押してください
 
 SET /P INPUTSTR=""
 
-copy %INPUTSTR% C:\Users\%username%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\RoamingState\project-halcyon\configurations
+copy %INPUTSTR% C:\Users\user\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\RoamingState\Vector\configuration
 
 echo 完了しました
 pause
